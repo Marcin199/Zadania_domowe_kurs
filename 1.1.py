e9569265859dd9bd9e1f71dee27d5540aaa5606e
Marcin <marcin.bugajewski@intelight.pl>
2020-04-30 21:48:26 +0200

    zadanie 1.1 - poprawki

--- 1.1.py
+++ 1.1.py
@@ -1,40 +1,40 @@
 class IleKosztuje:
     def policz(self):
-        zm_1 = float (input("Dzień Dobry, prosze podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
-        zm_2 = zm_1 * 5
-        print(f"Za pięć kilogramów ziemniaków trzeba będzie zapłacić {zm_2} zł. ")
+        ziemniaki = float (input("Dzień Dobry, prosze podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
+        cena = ziemniaki * 5
+        print(f"Za pięć kilogramów ziemniaków trzeba będzie zapłacić {cena} zł. ")
 
 Ile_Kosztuje = IleKosztuje()
 Ile_Kosztuje.policz()
 
 class IleKosztuje_IleKupuje:
     def policz(self):
-        zm_1 = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
-        zm_2 = float(input("Teraz, proszę podaj ile kilogramów ziemniaków chcesz kupić ? : "))
-        zm_3 = zm_1 * zm_2
-        print(f"Drogi kliencie, za te ziemniaki trzeba będzie zapłacić {zm_3} zł. ")
+        ziemniaki = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
+        kilogramy = float(input("Teraz, proszę podaj ile kilogramów ziemniaków chcesz kupić ? : "))
+        suma = ziemniaki * kilogramy
+        print(f"Drogi kliencie, za te ziemniaki trzeba będzie zapłacić {suma} zł. ")
 
 IleKosztuje_IleKupuje_1 = IleKosztuje_IleKupuje()
 IleKosztuje_IleKupuje_1.policz()
 
 class Ziemniki_Banany:
     def policz(self):
-        zm_1 = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
-        zm_2 = float(input("Teraz, proszę podaj ile kilogramów ziemniaków chcesz kupić ? : "))
-        zm_3 = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram bananów : "))
-        zm_4 = float(input("Teraz, proszę podaj ile kilogramów bananów chcesz kupić ? : "))
+        ziemniaki = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram ziemniaków : "))
+        kilo_ziemniaki = float(input("Teraz, proszę podaj ile kilogramów ziemniaków chcesz kupić ? : "))
+        banany = float(input("Dzień dobry, proszę podaj cenę, ile kosztuje 1 kilogram bananów : "))
+        kilo_banany = float(input("Teraz, proszę podaj ile kilogramów bananów chcesz kupić ? : "))
 
-        zm_ziem = zm_1*zm_2
-        zm_banan = zm_3*zm_4
-        zm_5 = zm_ziem + zm_banan
+        zm_ziem = ziemniaki*kilo_ziemniaki
+        zm_banan = banany*kilo_banany
+        suma_zakupy = zm_ziem + zm_banan
 
         if zm_ziem > zm_banan:
-            zm_6 = "Ziemniaki"
+            produkt = "Ziemniaki"
         else:
-            zm_6 = "Banany"
+            produkt = "Banany"
         #zcw = max(zm_ziem, zm_banan)
-        print(f"Drogi kliencie, razem za banany oraz ziemniaki trzeba będzie zapłacić {zm_5} zł. ")
-        print(f"Więcej trzeba bedzie zapłacić za {zm_6}.")
+        print(f"Drogi kliencie, razem za banany oraz ziemniaki trzeba będzie zapłacić {suma_zakupy} zł. ")
+        print(f"Więcej trzeba bedzie zapłacić za {produkt}.")
 
 Ziemniki_Banany_1 = Ziemniki_Banany()
 Ziemniki_Banany_1.policz()
