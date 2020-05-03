078f66fb3b4a78f2f4a8927a4af9c5ee16c5fc56
Marcin <marcin.bugajewski@intelight.pl>
2020-05-03 14:52:28 +0200

    zadanie 1.7 - poprawki

--- 1.7.py
+++ 1.7.py
@@ -4,20 +4,22 @@ Przykład:
 Co chcesz kupić? - ziemniaki
 Podaj cenę towaru - 5
 Podaj ilość towaru - 10
-​
+
 Za ziemniaki, który chcesz kupić, zapłacisz 50 zł
 """
 class Zakupy:
     def policz(self):
-        zm_1 = input("Dzień dobry, co chcesz kupić ? ")
-        zm_2 = float(input("Podaj cenę towaru : "))
-        zm_3 = float(input("Podaj ilość towaru :"))
-
-        if zm_1 == "ziemniaki" or zm_1 == "Ziemniaki":
-            suma = zm_2 * zm_3
-            print(f"Za ziemniaki, które chcesz kupić , zapłacisz {suma} zł.")
-        else:
-            print("W bazie nie ma takiego towaru. Poproś sprzedawcę o pomoc.")
+        zakupy = input("Dzień dobry, co chcesz kupić ? ")
+        cena_towaru = float(input("Podaj cenę towaru : "))
+        ilosc = float(input("Podaj ilość towaru :"))
+        try:
+            if zakupy == "ziemniaki" or zakupy == "Ziemniaki":
+                suma = cena_towaru * ilosc
+                print(f"Za ziemniaki, które chcesz kupić , zapłacisz {suma} zł.")
+            else:
+                print("W bazie nie ma takiego towaru. Poproś sprzedawcę o pomoc.")
+        except:
+            print("Brak towaru w bazie.Skontaktuj się z pracownikiem sklepu. ")
 
 
 Zakupy_1 = Zakupy()
